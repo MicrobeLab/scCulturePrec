bcb02a44cb702ede55126a0b9a3c604bdace09f1
MicrobeLab <52490989+MicrobeLab@users.noreply.github.com>
2026-03-02 15:11:44 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='scCulturePrec',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'torch',
+        'torchvision',
+        'h5py',
+        'scikit-learn',
+        'joblib',
+        'shap',
+    ],
+    entry_points={
+        'console_scripts': [
+            'scCulturePrec = scCulturePrec.runner:main',
+        ],
+    },
+)
+
